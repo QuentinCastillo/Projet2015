a93a82f290242ed889e0ed16962da5abbbd8d9ea
Christophe Gala <chris.gala@hotmail.fr>
2016-03-30 08:56:57 +0200

    .

--- src/fr/univavignon/courbes/network/central/PublicGameConnection.java
+++ src/fr/univavignon/courbes/network/central/PublicGameConnection.java
@@ -13,7 +13,7 @@ public class PublicGameConnection {
 	
 	public static void createPublicGame()
     {
-        String query="Insert into publicGame(IP, nb_joueurs, nb_joueurs_max) values(?, 1, 6)";
+        String query="Insert into partie(IP, nb_joueurs, nb_joueurs_max) values(?, 1, 6)";
         PreparedStatement prepare;
         Connection connection = PlayerConnection.connect();
         try
@@ -29,7 +29,8 @@ public class PublicGameConnection {
             try {
                 prepare = connection.prepareStatement(query);
                 prepare.setString(1, Ip.getHostAddress());
-                prepare.executeQuery();
+                prepare.executeUpdate();
+                prepare.close();
             } catch (SQLException e) {
                 // TODO Auto-generated catch block
             }
