10359d53fe7b6fa5657a29e29796dd3f96bd3586
vio282 <mary.emilie@outlook.fr>
2016-04-20 10:49:42 +0200

    Ajout package + agentimpl.java + méthodes à implémenter

---
+++ src/fr/univavignon/courbes/agents/groupe04/AgentImpl.java
@@ -0,0 +1,27 @@
+package fr.univavignon.courbes.agents.groupe04;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import fr.univavignon.courbes.agents.Agent;
+import fr.univavignon.courbes.common.Board;
+import fr.univavignon.courbes.common.Direction;
+import fr.univavignon.courbes.common.Position;
+import fr.univavignon.courbes.common.Snake;
+
+public class AgentImpl extends Agent
+{
+
+	public AgentImpl(Integer playerId) 
+	{
+		super(playerId);
+		
+	}
+
+	@Override
+	public Direction processDirection() {
+		
+		return null;
+	}	
+	
+}
